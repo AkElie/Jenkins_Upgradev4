acf87b8ece8a86dd6ba473a4e53a931796f8b472
a <a@a.com>
2021-10-07 07:02:52 -0400

    added more happy

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,8 +36,11 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me more Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+
+
 }
